b23d3959bbf15a02e53bca01d8cfab5af8673795
DELTATRE\Jeff.Huang <jeff.huang@deltatre.it>
2018-11-02 13:44:48 +0000

    download packages and setup project for selenium and UI testing

---
+++ tests/CreditCards.UITests/CreditCardApplicationtests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Xunit;
+
+namespace CreditCards.UITests
+{
+    public class CreditCardApplicationTests
+    {
+    }
+}
