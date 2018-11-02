a93dbc79242b7ae1ebba4e5a45d55ef4f10d8bc8
DELTATRE\Jeff.Huang <jeff.huang@deltatre.it>
2018-11-02 13:59:33 +0000

    test the app page loads in a web browser

--- tests/CreditCards.UITests/CreditCardApplicationtests.cs
+++ tests/CreditCards.UITests/CreditCardApplicationtests.cs
@@ -5,7 +5,28 @@ using Xunit;
 
 namespace CreditCards.UITests
 {
-    public class CreditCardApplicationTests
+    public class CreditCardApplicationTests : IDisposable
     {
+        private readonly IWebDriver _driver;
+
+        public CreditCardApplicationTests()
+        {
+            _driver = new ChromeDriver();
+        }
+
+        [Fact]
+        public void ShouldLoadApplicationPage_SmokeTest()
+        {
+            _driver.Navigate().GoToUrl("http://localhost:44108/apply");
+
+            Assert.Equal("Credit Card Application - CreditCards", _driver.Title);
+        }
+
+        public void Dispose()
+        {
+            // close any open web browser instance and dispose of it
+            _driver.Quit();
+            _driver.Dispose();
+        }
     }
 }
