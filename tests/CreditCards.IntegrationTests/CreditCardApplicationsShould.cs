366a90a1879343fbdc78c706902bd20c24ffd681
DELTATRE\Jeff.Huang <jeff.huang@deltatre.it>
2018-11-02 12:18:07 +0000

    move setup of test server into a shared location

--- tests/CreditCards.IntegrationTests/CreditCardApplicationsShould.cs
+++ tests/CreditCards.IntegrationTests/CreditCardApplicationsShould.cs
@@ -13,28 +13,19 @@ using Xunit;
 
 namespace CreditCards.IntegrationTests
 {
-    public class CreditCardApplicationsShould
+    public class CreditCardApplicationsShould : IClassFixture<TestServerFixture>
     {
-        private const string AntiForgeryFieldName = "__AFTField";
-        private const string AntiForgeryCookieName = "AFTCookie";
+        private readonly TestServerFixture _fixture;
+
+        public CreditCardApplicationsShould(TestServerFixture fixture)
+        {
+            _fixture = fixture;
+        }
 
         [Fact]
         public async Task RenderApplicationForm()
         {
-            // Build a Test Server
-            var builder = new WebHostBuilder()
-                // path to the CreditCards project
-                .UseContentRoot(@"C:\Users\jeff.huang\Documents\education\aspdotnet-core-mvc-testing-fundamentals\testing-fundamentals-project\src\CreditCards")
-                .UseEnvironment("Development")
-                .UseStartup<CreditCards.Startup>()
-                .UseApplicationInsights();
-
-            var server = new TestServer(builder);
-
-            // create the HTTP client
-            var client = server.CreateClient();
-
-            var response = await client.GetAsync("/Apply");
+            var response = await _fixture.Client.GetAsync("/Apply");
 
             response.EnsureSuccessStatusCode();
 
@@ -43,90 +34,39 @@ namespace CreditCards.IntegrationTests
             Assert.Contains("New Credit Card Application", responseString);
         }
 
+        // This tests uses anti-forgery tokens since the post method requires a correct token to successfully POST
         [Fact]
         public async Task NotAcceptPostedApplicationDetailsWithMissingFrequentFlyerNumber()
         {
-            var builder = new WebHostBuilder()
-                .UseContentRoot(@"C:\Users\jeff.huang\Documents\education\aspdotnet-core-mvc-testing-fundamentals\testing-fundamentals-project\src\CreditCards")
-                .UseEnvironment("Development")
-                .UseStartup<CreditCards.Startup>()
-                .ConfigureServices(x =>
-                {
-                    x.AddAntiforgery(t =>
-                    {
-                        t.CookieName = AntiForgeryCookieName;
-                        t.FormFieldName = AntiForgeryFieldName;
-                    });
-                })
-                .UseApplicationInsights();
-
-            var server = new TestServer(builder);
-            var client = server.CreateClient();
-
             // Get initial response that contains anti forgery tokens
-            HttpResponseMessage initialResponse = await client.GetAsync("/Apply");
-
-            string antiForgeryCookieValue = ExtractAntiForgeryCookieValueFrom(initialResponse);
-            string antiForgeryToken = ExtractAntiForgeryToken(await initialResponse.Content.ReadAsStringAsync());
+            HttpResponseMessage initialResponse = await _fixture.Client.GetAsync("/Apply");
+            var antiForgeryValues = await _fixture.ExtractAntiForgeryValues(initialResponse);
 
 
             // Create POST request, adding anti forgery cookie and form field
-            HttpRequestMessage postRequest = new HttpRequestMessage(HttpMethod.Post, "/Apply");
+            var postRequest = new HttpRequestMessage(HttpMethod.Post, "/Apply");
 
             postRequest.Headers.Add("Cookie",
-                new CookieHeaderValue(AntiForgeryCookieName, antiForgeryCookieValue).ToString());
+                new CookieHeaderValue(TestServerFixture.AntiForgeryCookieName,
+                                      antiForgeryValues.cookieValue).ToString());
 
             var formData = new Dictionary<string, string>
             {
-                {AntiForgeryFieldName, antiForgeryToken},
+                {TestServerFixture.AntiForgeryFieldName, antiForgeryValues.fieldValue},
                 {"FirstName", "Sarah"},
                 {"LastName", "Smith"},
                 {"Age", "18"},
                 {"GrossAnnualIncome", "100000"}
-                // Frequent flyer number omitted to check we can't proceed with the application
+                // Frequent flyer number omitted
             };
 
             postRequest.Content = new FormUrlEncodedContent(formData);
 
-            HttpResponseMessage postResponse = await client.SendAsync(postRequest);
-
+            var postResponse = await _fixture.Client.SendAsync(postRequest);
             postResponse.EnsureSuccessStatusCode();
 
             var responseString = await postResponse.Content.ReadAsStringAsync();
-
             Assert.Contains("Please provide a frequent flyer number", responseString);
-        }
-
-        private static string ExtractAntiForgeryCookieValueFrom(
-            HttpResponseMessage response)
-        {
-            string antiForgeryCookie = response.Headers.GetValues("Set-Cookie")
-                .FirstOrDefault(x => x.Contains(AntiForgeryCookieName));
-
-            if (antiForgeryCookie is null)
-            {
-                throw new ArgumentException(
-                    $"Cookie '{AntiForgeryCookieName}' not found in HTTP response",
-                    nameof(response));
-            }
-
-            string antiForgeryCookieValue =
-                SetCookieHeaderValue.Parse(antiForgeryCookie).Value;
-
-            return antiForgeryCookieValue;
-        }
-
-        private string ExtractAntiForgeryToken(string htmlBody)
-        {
-            var requestVerificationTokenMatch =
-                Regex.Match(htmlBody, $@"\<input name=""{AntiForgeryFieldName}"" type=""hidden"" value=""([^""]+)"" \/\>");
-
-            if (requestVerificationTokenMatch.Success)
-            {
-                return requestVerificationTokenMatch.Groups[1].Captures[0].Value;
-            }
-
-            throw new ArgumentException($"Anti forgery token '{AntiForgeryFieldName}' not found in HTML", nameof(htmlBody));
         }
     }
 }
